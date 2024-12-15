5c8ed258bbf8126981090096eac54569f6d7e9df
DexterDC <sebasdc9@hotmail.com>
2024-12-15 12:26:14 -0600

    remove search bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -29,29 +29,6 @@ function Header() {
           </p>
         </div>
       </article>
-      <article className="relative w-full max-w-xl">
-        <div className="text-white text-opacity-50 rounded-full p-2 absolute top-0 left-1">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="size-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
-            />
-          </svg>
-        </div>
-        <input
-          type="text"
-          className="w-full px-4 py-2 pl-10 bg-gray-800 border border-gray-700 rounded-lg focus:outline-none focus:border-purple-500 text-gray-100"
-          placeholder="Search for artists, albums, or genres..."
-        />
-      </article>
     </header>
   );
 }
